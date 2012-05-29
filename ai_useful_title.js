9a087bb110b0808383f17e29a3883bbe938be63f
Federico <fedmich@gmail.com>
2012-05-29 17:33:03 +0800

    more patterns

--- ai_useful_title.js
+++ ai_useful_title.js
@@ -1,10 +1,16 @@
-//Returns a string after cleaning junks, for use as post title
+/*
+ * Returns a string after cleaning junks, for use as post title
+ */
 function ai_useful_title ( str ){
+	if(! str){
+		return '';
+	}
 	var ret = decodeURI( str );
 	
 	//trim extra spaces
 	ret = ret.replace( /^\s+|\s+$/g , '');
 	switch(ret){
+	case 'I':
 	case '|':
 		//string not too useful
 		return '';
@@ -18,7 +24,12 @@ function ai_useful_title ( str ){
 		}
 	}
 	
-	if( ret.match(/DSCN\d\d.*/i)){
+	if(ret.match(/tumblr.*jpg/i)){
+		//filename not too useful
+		return '';
+	}
+	
+	if( ret.match(/(DSCN\d\d.*|DSC\d\d.*)/i)){
 		return ''; //Camera pictures
 	}
 	
