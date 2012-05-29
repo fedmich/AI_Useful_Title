96fd56fa2d2e80465a305cee5f827bd45514150c
Federico <fedmich@gmail.com>
2012-05-29 17:11:55 +0800

    JS, ai_useful_title
    0.1

---
+++ ai_useful_title.js
@@ -0,0 +1,48 @@
+//Returns a string after cleaning junks, for use as post title
+function ai_useful_title ( str ){
+	var ret = decodeURI( str );
+	
+	//trim extra spaces
+	ret = ret.replace( /^\s+|\s+$/g , '');
+	switch(ret){
+	case '|':
+		//string not too useful
+		return '';
+	}
+	
+	var ms_tcount = ret.match(/\d/g);
+	if( ms_tcount ){
+		if( ms_tcount.length >  16){
+			//if there are more than 16 numbers, don't use this as title
+			return '';
+		}
+	}
+	
+	if( ret.match(/DSCN\d\d.*/i)){
+		return ''; //Camera pictures
+	}
+	
+	//remove extension
+	ret = ret.replace(/\.(jpeg|jpg|png|gif)$/i,'');
+	
+	//if ending with dot, remove dot.
+	ret = ret.replace(/\.$/i,'');
+	
+	//underscore to space
+	ret = ret.replace(/_/g,' ');
+	
+	//replace characters + to space
+	ret = ret.replace( /[+]/g , ' ');
+	
+	//replace double-space to space
+	ret = ret.replace( /  /g , ' ');
+	
+	//trim extra spaces
+	ret = ret.replace( /^\s+|\s+$/g , '');
+	
+	//If chars is less than 3 chars, it's not too useful.
+	if( ret.length < 3){
+		return ''; //too short
+	}
+	return ret;
+}
